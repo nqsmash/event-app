e414f7e8eadaa30014a55ac524341e5fe17c7900
nqsmash <nqsmash@proton.me>
2024-01-21 05:09:16 -0500

    Verify Device has NFC Capability

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import NfcManager from 'react-native-nfc-manager';
 
 export default function App() {
+  const [hasNfc, setHasNfc] = React.useState<boolean | null>(null);
+
+  React.useEffect(() => {
+    async function checkNfc() {
+      setHasNfc(await NfcManager.isSupported());
+    }
+
+    checkNfc();
+  }, []);
+
+  if (hasNfc === null) {
+    return null;
+  } else if (!hasNfc) {
+    return (
+      <View style={styles.container}>
+        <Text>Your device doesn't support NFC</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your testapp!</Text>
+      <Text>Hello NFCs!</Text>
       <StatusBar style="auto" />
     </View>
   );
